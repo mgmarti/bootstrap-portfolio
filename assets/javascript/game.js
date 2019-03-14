4bb78bbc40e557a17faa7710c6faa39385e8c0bc
moma <mogimartinez@gmail.com>
2019-03-13 20:09:41 -0500

    linked portfolio.html to guess the letter site

---
+++ assets/javascript/game.js
@@ -0,0 +1,60 @@
+var guesses = ['a','b','c','d','e','f','g','h','i','j','k','l',
+'m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+console.log(guesses)
+
+var wins = 0;
+var losses = 0;
+var guessesRemaining = 10;
+var guessedLetters = [];
+
+console.log(wins)
+console.log(losses)
+console.log(guessesRemaining)
+console.log(guessedLetters)
+
+ // Create variables that hold references to the places in the HTML where we want to display things.
+ var directionsText = document.getElementById("directions-text");
+ var winsText = document.getElementById("wins-text");
+ var lossesText = document.getElementById("losses-text");
+ var guessedLettersText = document.getElementById("guessedLetters-text");
+ var guessesRemainingText = document.getElementById("guessesRemaining-text");
+
+// Randomly chooses a choice from the options array. This is the Computer's guess.
+var computerGuess = guesses[Math.floor(Math.random() * guesses.length)];
+console.log(computerGuess)
+
+// This function is run whenever the user presses a key.
+document.onkeyup = function(event) {
+var userGuess = event.key;
+
+// Determines which key was pressed.
+
+console.log(userGuess)
+if (userGuess === computerGuess) {
+    console.log("You won!")
+    wins++;
+    computerGuess = guesses[Math.floor(Math.random() * guesses.length)];
+
+
+} else if (userGuess !== computerGuess) {
+    guessesRemaining--;
+    guessedLetters.innerHTML = guessedLetters.join(',');
+}
+
+if (guessesRemaining === 0) {
+    losses++
+    computerGuess = guesses[Math.floor(Math.random() * guesses.length)];
+
+}
+
+
+// Hide the directions
+directionsText.textContent = "";
+
+// Display the user and computer guesses, and wins/losses/ties.
+winsText.textContent = "wins: " + wins;
+lossesText.textContent = "losses: " + losses;
+guessesRemainingText.textcontent = "Guesses remaining: " + guessesRemaining;
+guessedLettersText.textContent = "Your guesses so far: " + userGuess;
+}
+
